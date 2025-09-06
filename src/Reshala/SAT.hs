30258025afa90d9b72ea3c3fbb876292efc921b9
thelissimus <thelissimus@tuta.io>
2025-09-07 01:11:42 +0500

    add: `polarity` & `elim`

--- src/Reshala/SAT.hs
+++ src/Reshala/SAT.hs
@@ -8,13 +8,15 @@ module Reshala.SAT
   , subst
   , simp
   , sat
+  , polarity
+  , elim
   ) where
 
 import Control.Applicative ((<|>))
 import Data.Data (Data)
 import Data.Functor.Foldable
 import Data.Functor.Foldable.TH
-import Data.Maybe (fromMaybe)
+import Data.Maybe (catMaybes, fromMaybe, mapMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
 import Pre
@@ -97,6 +99,39 @@ distributeCNF = transform (apply distribute)
 
 normalize :: Expr -> Expr
 normalize = simp . distributeCNF . toNNF . simp
+
+data Polarity = Pos | Neg | Mix
+  deriving stock (Eq)
+
+polarity :: Expr -> Char -> Maybe Polarity
+polarity (Var v) v'
+  | v == v' = Just Pos
+  | otherwise = Nothing
+polarity (Not (Var v)) v'
+  | v == v' = Just Neg
+  | otherwise = Nothing
+polarity expr v = case expr of
+  a :& b -> comb [a, b]
+  a :| b -> comb [a, b]
+  Not a -> error $ "polarity: not in CNF: negation of a non-literal: " ++ show a
+  Lit _ -> Nothing
+ where
+  comb es = case mapMaybe (`polarity` v) es of
+    [] -> Nothing
+    ps -> if all (== Pos) ps then Just Pos else if all (== Neg) ps then Just Neg else Just Mix
+
+elim :: Expr -> Expr
+elim expr =
+  let ls = toList (vars expr)
+      ps = map (polarity expr) ls
+      as = catMaybes (zipWith extract ls ps)
+      rs = map (uncurry subst) as
+   in foldl' (.) id rs expr
+ where
+  extract v = \case
+    Just Pos -> Just (v, True)
+    Just Neg -> Just (v, False)
+    _ -> Nothing
 
 sat :: Expr -> Bool
 sat expr = case free expr of
