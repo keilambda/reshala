8231855f8dbec8cc068ef24378dc45426e1ebfdb
thelissimus <thelissimus@tuta.io>
2025-09-06 23:05:52 +0500

    add: `sat`

--- src/Pre.hs
+++ src/Pre.hs
@@ -1,7 +1,13 @@
 module Pre
   ( module Prelude
+  , module Data.Foldable
+  , module Control.Lens
+  , Generic
   , List
   ) where
 
+import Control.Lens
+import Data.Foldable
 import Data.List (List)
+import GHC.Generics (Generic)
 import Prelude
--- src/Reshala/SAT.hs
+++ src/Reshala/SAT.hs
@@ -1,5 +1,20 @@
-module Reshala.SAT () where
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
+module Reshala.SAT
+  ( Expr (..)
+  , Clause
+  , Formula
+  , free
+  , guess
+  , simp
+  , sat
+  ) where
+
+import Control.Applicative ((<|>))
+import Data.Data (Data)
+import Data.Functor.Foldable
+import Data.Functor.Foldable.TH
 import Pre
 
 data Expr
@@ -8,9 +23,56 @@ data Expr
   | Not Expr
   | Expr :& Expr
   | Expr :| Expr
+  deriving stock (Data, Eq, Generic, Show)
+  deriving anyclass (Plated)
 
 infixl 7 :&
 infixl 6 :|
 
+makeBaseFunctor ''Expr
+
 type Clause = List Expr
 type Formula = List Clause
+
+free :: Expr -> Maybe Char
+free = cata alg
+ where
+  alg = \case
+    LitF _ -> Nothing
+    VarF v -> Just v
+    NotF a -> a
+    a :&$ b -> a <|> b
+    a :|$ b -> a <|> b
+
+guess :: Char -> Bool -> Expr -> Expr
+guess var val = cata alg
+ where
+  alg (VarF v) | v == var = Lit val
+  alg a = embed a
+
+step :: Expr -> Maybe Expr
+step = \case
+  (Not (Lit b)) -> Just (Lit (not b))
+  (Not (Not a)) -> Just a
+  (Lit True :& a) -> Just a
+  (a :& Lit True) -> Just a
+  (Lit False :& _) -> Just (Lit False)
+  (_ :& Lit False) -> Just (Lit False)
+  (Lit False :| a) -> Just a
+  (a :| Lit False) -> Just a
+  (Lit True :| _) -> Just (Lit True)
+  (_ :| Lit True) -> Just (Lit True)
+  _ -> Nothing
+
+simp :: Expr -> Expr
+simp = rewrite step
+
+sat :: Expr -> Bool
+sat expr = case free expr of
+  Nothing -> unLit expr
+  Just v ->
+    let true = simp (guess v True expr)
+        false = simp (guess v False expr)
+     in sat true || sat false
+  where
+    unLit = \case Lit b -> b; _ -> error "unLit: not a Lit"
