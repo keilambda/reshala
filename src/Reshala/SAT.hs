42a70c0965626bc4ff4f77cbe4a750e8212d6ff1
thelissimus <thelissimus@tuta.io>
2025-09-06 22:10:57 +0500

    add: `Expr` definition

--- src/Reshala/SAT.hs
+++ src/Reshala/SAT.hs
@@ -1,1 +1,16 @@
 module Reshala.SAT () where
+
+import Pre
+
+data Expr
+  = Lit Bool
+  | Var Char
+  | Not Expr
+  | Expr :& Expr
+  | Expr :| Expr
+
+infixl 7 :&
+infixl 6 :|
+
+type Clause = List Expr
+type Formula = List Clause
