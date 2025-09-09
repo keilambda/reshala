cceac160e9bda36386448bec71209739bdc52543
thelissimus <thelissimus@tuta.io>
2025-09-09 19:07:07 +0500

    add: `Type` and `Constraint` to prelude

--- src/Pre.hs
+++ src/Pre.hs
@@ -6,10 +6,13 @@ module Pre
   , List
   , NESet
   , Set
+  , Type
+  , Constraint
   ) where
 
 import Control.Lens
 import Data.Foldable
+import Data.Kind (Constraint, Type)
 import Data.List (List)
 import Data.Set (Set)
 import Data.Set.NonEmpty (NESet)
