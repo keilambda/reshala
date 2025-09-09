a21256b10678c59f57cb98b79b2bb3e91b5a0ac7
thelissimus <thelissimus@tuta.io>
2025-09-10 04:56:26 +0500

    impl: `Table` solver implementation

--- src/Reshala/SAT/Solver/Table.hs
+++ src/Reshala/SAT/Solver/Table.hs
@@ -1,1 +1,46 @@
-module Reshala.SAT.Solver.Table () where
+module Reshala.SAT.Solver.Table
+  ( sat
+  , solutions
+  ) where
+
+import Control.Monad (replicateM)
+import Data.Functor.Foldable
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
+import Pre
+import Reshala.SAT
+import Reshala.SAT.Expr
+
+type Solution = Map Var Bool
+
+eval :: Solution -> Expr -> Bool
+eval env = cata \case
+  LitF b -> b
+  VarF v -> Map.findWithDefault False v env
+  NotF b -> not b
+  a :&:$ b -> a && b
+  a :|:$ b -> a || b
+
+vars :: Expr -> Set Var
+vars = foldMap alg . universe
+ where
+  alg = \case
+    Var v -> Set.singleton v
+    _ -> mempty
+
+assignments :: List Var -> List Solution
+assignments vs =
+  [ Map.fromList (zip vs bs)
+  | bs <- replicateM (length vs) [False, True]
+  ]
+
+solutions :: Expr -> List Solution
+solutions e = filter (`eval` e) . assignments . toList . vars $ e
+
+sat :: Expr -> Bool
+sat = not . null . solutions
+
+newtype Table = MkTable Expr
+
+instance Solver Table where
+  solve (MkTable e) = sat e
