1fc325a877136991cdfaa188270652132c34cc92
thelissimus <thelissimus@tuta.io>
2025-09-09 19:45:59 +0500

    add: `Map` to prelude

--- src/Pre.hs
+++ src/Pre.hs
@@ -4,6 +4,7 @@ module Pre
   , module Control.Lens
   , Generic
   , List
+  , Map
   , NESet
   , Set
   , Type
@@ -14,6 +15,7 @@ import Control.Lens
 import Data.Foldable
 import Data.Kind (Constraint, Type)
 import Data.List (List)
+import Data.Map.Strict (Map)
 import Data.Set (Set)
 import Data.Set.NonEmpty (NESet)
 import GHC.Generics (Generic)
